d914438d1a4de391cefced5f3ecd4c6f7c38467c
mjbe-bikes <mjbebikes@gmail.com>
2026-09-14 09:33:47 -0500

    Arreglo de archivos para iniciar con rutas

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ import mysql from "mysql2/promise.js";
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: '',
     database: process.env.DB_NAME,
-    waitforConnections: true,
+    waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
